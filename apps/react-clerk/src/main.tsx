34f2e4a8cc662112532c8aca9f471cbe3a97dd8e
JacobMGEvans <jacobmgevans@gmail.com>
2023-09-18 22:05:31 -0500

    CI Action & TypeScript

--- apps/react-clerk/src/main.tsx
+++ apps/react-clerk/src/main.tsx
@@ -10,7 +10,7 @@ import "./styles/index.css";
 
 const publishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={publishableKey}>
       <BrowserRouter>
